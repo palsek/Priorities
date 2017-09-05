e2ca162c04bf0f82116c9334b6e293c3ed21885f
palsek <palsek@gmail.com>
2017-09-05 14:31:31 +0200

    Extend filtering items add new class ViewInfo

---
+++ AspNetIdentityTry1/Models/ViewInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetIdentityTry1.Models
+{
+    public class ViewInfo
+    {
+        public int Page { get; set; }
+        public int NumberPerPage { get; set; }
+        public string OrderBy { get; set; }
+        public string OrderDirection { get; set; }
+        public int AllPageNumber { get; set; }
+
+        // for admin functionality
+        public List<string> AllUsersName { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
