0dfecfda8c2fb61f3934d202043eed8935da8f6f
palsek <palsek@gmail.com>
2017-09-21 20:33:43 +0200

    User infor with UserDelete 2

--- AspNetIdentityTry1/Models/User.cs
+++ AspNetIdentityTry1/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,30 @@ namespace AspNetIdentityTry1.Models
 {
     public class User : IdentityUser
     {
+        [Display(Name = "User name")]
+        public override string UserName
+        {
+            get
+            {
+                return base.UserName;
+            }
+            set
+            {
+                base.UserName = value;
+            }
+        }
+
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
         public string Position { get; set; }
+
         public string Email { get; set; }
+
+        [Display(Name = "Parent name")]
         public string ParentName { get; set; }
        // public ICollection<Item> Items { get; set; }
     }
